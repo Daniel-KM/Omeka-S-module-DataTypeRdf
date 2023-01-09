5ce98efc275255a9089a85a571473e2410261c4c
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-01-09 00:00:00 +0000

    Used Omeka S v4 feature "element group" for settings.

--- src/Form/SettingsFieldset.php
+++ src/Form/SettingsFieldset.php
@@ -9,14 +9,20 @@ class SettingsFieldset extends Fieldset
 {
     protected $label = 'Data Type Rdf'; // @translate
 
+    protected $elementGroups = [
+        'resources' => 'Resources', // @translate
+    ];
+
     public function init(): void
     {
         $this
             ->setAttribute('id', 'data-type-rdf')
+            ->setOption('element_groups', $this->elementGroups)
             ->add([
                 'name' => 'datatyperdf_html_mode_resource',
                 'type' => DataTypeRdfElement\OptionalRadio::class,
                 'options' => [
+                    'element_group' => 'resources',
                     'label' => 'Html edition mode for resources', // @translate
                     'value_options' => [
                         'inline' => 'Inline (default)', // @translate
@@ -31,6 +37,7 @@ class SettingsFieldset extends Fieldset
                 'name' => 'datatyperdf_html_config_resource',
                 'type' => DataTypeRdfElement\OptionalRadio::class,
                 'options' => [
+                    'element_group' => 'resources',
                     'label' => 'Html edition config and toolbar for resources', // @translate
                     'value_options' => [
                         // @see https://ckeditor.com/cke4/presets-all
