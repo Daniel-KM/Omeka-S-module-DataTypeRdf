7435a5cbc38dc87eccfb9596bacf43da6652fc7d
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-07-08 00:00:00 +0000

    Updated html check to do a basic check only.

--- src/DataType/Html.php
+++ src/DataType/Html.php
@@ -77,7 +77,11 @@ class Html extends AbstractDataTypeRdf
     public function isValid(array $valueObject)
     {
         return isset($valueObject['@value'])
-            && self::isWellFormed($valueObject['@value'])
+            // Skip check well-formed because php < 8.4 does not support html 5.
+            // && self::isWellFormed($valueObject['@value'])
+            // Nevertheless, do a quick check, because purify may be skipped in
+            // the config.
+            && self::isWellFormedBasic($valueObject['@value'])
             && $this->htmlPurifier->purify(trim((string) $valueObject['@value'])) !== '';
     }
 
@@ -129,6 +133,8 @@ class Html extends AbstractDataTypeRdf
      * Check if a string is a well-formed html with start/end tags for any part.
      * Don't check validity or security.
      *
+     * Because php < 8.4 does not manage html 5, the check is basic.
+     *
      * Support strings without a root tag, but require a tag for each fragment
      * of the string. So a simple string is not html, even if it contains valid
      * html tags.
@@ -138,7 +144,7 @@ class Html extends AbstractDataTypeRdf
      * @example `This is <strong>not</strong> a valid html for this function.`
      * @example `This is not a valid html for this function.`
      */
-    public static function isWellFormed($string): bool
+    public static function isWellFormedBasic($string): bool
     {
         if (!$string) {
             return false;
@@ -157,35 +163,11 @@ class Html extends AbstractDataTypeRdf
         if (!$string
             || mb_substr($string, 0, 1) !== '<'
             || mb_substr($string, -1) !== '>'
-            // TODO Is it really a quick check to use strip_tags before domdocument?
             || $string === strip_tags($string)
         ) {
             return false;
         }
 
-        // With CKeditor or CodeMirror, the root node is not required, so append
-        // one. Anyway, it is required by the specification for html fragment.
-        // False is already returned above for simple strings.
-        // For Html, it should be html.
-        // Normally, libXml add missing html/body, etc., except with flags
-        // LIBXML_HTML_NOIMPLIED and LIBXML_HTML_NODEFDTD,
-        // but it work better when manually added.
-        if (mb_substr($string, 0, 9) !== '<!DOCTYPE'
-            && mb_substr($string, 0, 5) !== '<html'
-            && mb_substr($string, -7) !== '</html>'
-        ) {
-            $string = '<html>' . $string . '</html>';
-        }
-
-        libxml_use_internal_errors(true);
-        libxml_clear_errors();
-        $dom = new \DOMDocument();
-        $result = $dom->loadHTML(
-            $string,
-            LIBXML_COMPACT | LIBXML_NONET
-        );
-
-        return $result
-            && !count(libxml_get_errors());
+        return true;
     }
 }
